68de43c93eab778cde9d0d3a966056dc9e6e9054
KenTra222 <Kentraviousc@gmail.com>
2022-07-01 22:13:12 +0000

    configured storybook main js to add my components

--- twitter-clone/.storybook/main.js
+++ twitter-clone/.storybook/main.js
@@ -1,13 +1,15 @@
 module.exports = {
   "stories": [
     "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+    "../Components/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook-addon-sass-postcss"
+    '@storybook/addon-a11y',
+    '@storybook/preset-scss'
   ],
   "framework": "@storybook/react",
   "core": {
